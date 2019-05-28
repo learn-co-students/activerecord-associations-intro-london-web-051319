c78aea5116f7fcb7d191374bda6a5325087ee675
Amy Quinn <amyequinn@hotmail.co.uk>
2019-05-28 10:45:16 +0100

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,13 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :genres, through: :songs
+
 end
+
+
+# app/models/user.rb
+#
+# class User < ActiveRecord::Base
+#   has_many :reviews
+#   has_many :hotels, through: :reviews
+# end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,13 @@
 class Genre < ActiveRecord::Base
+  has_many :songs
+  has_many :artists, through: :songs
+
 end
+
+
+# app/models/hotel.rb
+#
+# class Hotel<ActiveRecord::Base
+#   has_many :reviews
+#   has_many :users, through: :reviews
+# end
